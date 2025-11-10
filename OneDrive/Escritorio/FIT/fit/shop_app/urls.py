0f2cfc8e2ed7345bb162c54938768c77e9b3c51a
ImAlexG <alejandroescool2004@gmail.com>
2025-11-09 23:08:06 -0600

    test

---
+++ OneDrive/Escritorio/FIT/fit/shop_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path("products", views.products, name="products")
+]
+
+#fetching all_products: http://127.0.0.1.8000/products/
